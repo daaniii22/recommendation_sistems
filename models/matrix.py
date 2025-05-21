9ac71a3844ab8bab1c55c2c2b8812b21841843e8
Hakai69 <daniel.np.contact+gh@gmail.com>
2025-05-21 20:30:15 +0200

    add: matrix factorization model

--- models/matrix.py
+++ models/matrix.py
@@ -1,3 +1,71 @@
+from typing import Sequence
+from tqdm import tqdm
+import warnings
+
+import numpy as np
+from numpy.typing import ArrayLike
+
 
 class MatrixFactorization:
-    pass
+    
+    def __init__(self, n_factors: int = 7):
+        self.n_factors = n_factors
+        
+    def _check(self, u: int, i: int = None) -> None:
+        if not hasattr(self, 'p'):
+            raise ValueError('Model not fitted yet')
+        if not 0 <= u < len(self.p):
+            raise ValueError(f'User ID {u} out of range [0, {len(self.p)})')
+        if i is not None and not 0 <= i < len(self.q):
+            raise ValueError(f'Item ID {i} out of range [0, {len(self.q)})')
+        
+    def compute_prediction(self, u: int, i: int) -> float:
+        return np.dot(self.p[u], self.q[i]) + self.bu[u] + self.bi[i]
+    
+    def predict(self, u: int, i: int) -> float:
+        self._check(u, i)
+        return self.compute_prediction(u, i)
+        
+    def fit(
+        self,
+        U: ArrayLike | Sequence[int],
+        I: ArrayLike | Sequence[int],
+        ratings: ArrayLike | Sequence[float],
+        epochs: int = 10,
+        learning_rate: float = 0.001,
+        regularization: float = 0.1,
+        verbose: bool = False,
+    ):
+        if min(U) < 0 and max(U) >= len(U):
+            raise ValueError('User IDs must be in the range [0, len(U))')
+        if min(I) < 0 or max(I) >= len(I):
+            raise ValueError('Item IDs must be in the range [0, len(I))')
+        
+        self.p = np.random.rand(len(U), self.n_factors)
+        self.bu = np.random.rand(len(U))
+        self.q = np.random.rand(len(I), self.n_factors)
+        self.bi = np.random.rand(len(I))
+        
+        iterable = range(epochs) if not verbose else tqdm(range(epochs), desc='Training')
+        for _ in iterable:
+            for u, i, rating in zip(U, I, ratings):
+                prediction = self.predict(u, i)
+                error = rating - prediction
+                
+                self.p[u] += learning_rate * (error * self.q[i] - regularization * self.p[u])
+                self.q[i] += learning_rate * (error * self.p[u] - regularization * self.q[i])
+                self.bu[u] += learning_rate * (error - regularization * self.bu[u])
+                self.bi[i] += learning_rate * (error - regularization * self.bi[i])
+                
+    def predict_all(self, u: int) -> np.ndarray:
+        self._check(u)
+        return np.dot(self.p[u], self.q.T) + self.bu[u] + self.bi
+    
+    def get_recommendations(self, u: int, n: int = 10) -> np.ndarray:
+        predictions = self.predict_all(u)
+        if len(predictions) < n:
+            warnings.warn(f'Not enough items in dataset for {n} recommendations ({len(predictions)}).', RuntimeWarning)
+            return np.arange(len(predictions))
+        
+        recommendations = np.argpartition(predictions, -n)[-n:]
+        return recommendations
